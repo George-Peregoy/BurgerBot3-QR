64171adfd57604c937d2e9bb74d4ea3e13c989c3
George-Peregoy <georgeperegoyr@gmail.com>
2026-03-05 00:31:33 -0500

    Added dependecies

--- src/simulation/setup.py
+++ src/simulation/setup.py
@@ -10,12 +10,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        ('share/' + package_name + '/launch', 
+            ['launch/launch_sim.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='gperegoy',
     maintainer_email='georgeperegoyr@gmail.com',
-    description='TODO: Package description',
+    description='Models environment and burger bot in gazebo.',
     license='Apache-2.0',
     extras_require={
         'test': [
