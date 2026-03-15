7e61df069581a4d66be08bdb555faade84429647
George-Peregoy <georgeperegoyr@gmail.com>
2026-03-15 11:00:01 -0400

    Added world num launch arg

--- src/simulation/launch/launch_sim.py
+++ src/simulation/launch/launch_sim.py
@@ -3,12 +3,13 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetE
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
-    
+ 
     # create arg for model
     model_arg = DeclareLaunchArgument(
         'model',
@@ -16,26 +17,46 @@ def generate_launch_description():
         description='TurtleBot3 model type'
     )  
     
-    # create arg for world
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value='empty_world.world',
-        description='Empty gazebo world file'
+    # where world dir is kept
+    sim_share = get_package_share_directory('simulation')
+    set_model_path = SetEnvironmentVariable(
+        'GAZEBO_MODEL_PATH',
+        sim_share
     )
+    
+    # create arg for world
+    world_num_arg = DeclareLaunchArgument(
+        'world_num',
+        default_value='0',
+        description='Arg for which world to use.'
+        )
+
+    world_num = LaunchConfiguration('world_num')
+
+    world_file = PathJoinSubstitution([
+        sim_share,
+        'worlds',
+        ['world_', world_num, '.world']
+    ])
 
     # use env file associated with world 
+    path_share = get_package_share_directory('path_planning')
+    env_file = PathJoinSubstitution([
+        path_share,
+        'environments',
+        ['environment_polygon_', world_num, '.pickle']
+    ])
 
     path_publisher_node = Node(
         package = 'path_planning',
         executable = 'path_publisher',
         parameters = [{
-
+            'env_file': env_file
         }]
     )
 
     # resolve args
     model = LaunchConfiguration('model')
-    world = LaunchConfiguration('world')
 
     # set env variable for model
     set_turtlebot_model = SetEnvironmentVariable(
@@ -54,15 +75,23 @@ def generate_launch_description():
         ),
         launch_arguments={
             'model' : model,
-            'world' : world
+            'world' : world_file
         }.items()
 
     )
+
+    # Add this to see what's being passed
+    from launch.actions import LogInfo
+    log_world = LogInfo(
+        msg=['Loading world file: ', world_file]
+    )
     
     return LaunchDescription([
+        set_model_path,
         model_arg,
-        world_arg,
+        world_num_arg,
         set_turtlebot_model,
+        log_world,
         gazebo,
         path_publisher_node
     ])
