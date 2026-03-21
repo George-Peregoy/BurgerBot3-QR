0aa5f69932e6cb097ad51ad545c632c44484e372
George-Peregoy <georgeperegoyr@gmail.com>
2026-03-21 18:29:52 -0400

    Changed robot radius

--- src/path_planning/path_planning/config.py
+++ src/path_planning/path_planning/config.py
@@ -4,20 +4,16 @@ File used to store global variables.
 Notes
 -----
 File is currenly being used for TurtleBot3 Burgerbots.
-
-L x W x H is 138mm x 178mm x 192 mm as per https://robotis.us/turtlebot-3-burger-rpi4-4gb-us/?srsltid=AfmBOopVrxRs5QI4pK4bpUb24-oybPMQE-vqyCv-mMc6h-KLMzfUwSD6.
-
-Will use a radius of 178/2 mm -> 89mm -> 0.089m with an added buffer of radius + 10% of radius. 
 """
 
 ENV_X_BOUNDS = (0,50) 
 ENV_Y_BOUNDS = (0,50)
 BOUNDS = (ENV_X_BOUNDS, ENV_Y_BOUNDS)
 
-START = (45, 45)
-GOAL = (5, 5)
+START = (45, 45) # robot 1 start | robot 2 end
+GOAL = (5, 5) # robot 2 start | robot 1 end
 
-ROBOT_RADIUS = 0.089 # meters
+ROBOT_RADIUS = 0.105 # meters
 RADIUS_BUFFER = ROBOT_RADIUS * 0.1
 BUFFER = ROBOT_RADIUS + RADIUS_BUFFER # in meters
 
